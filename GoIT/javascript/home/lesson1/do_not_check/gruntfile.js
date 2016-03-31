abbb9725b60e1aead347fa3c13495463ed81fa88
Natalia Krasnovska gingerrN <flymodd@gmail.com>
2016-03-31 18:45:41 +0300

    not check

---
+++ GoIT/javascript/home/lesson1/do_not_check/gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'styles',
+          src: ['*.scss'],
+          dest: 'styles',
+          ext: '.css'
+        }]
+      }
+    },
+    watch: {
+      sass: {
+        // We watch and compile sass files as normal but don't live reload here
+        files: ['styles/*.scss'],
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass']);
+
+};
